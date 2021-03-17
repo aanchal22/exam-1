4bd67a477fd975f5d5583f3159aba154baafab52
bloombar <amos@cs.nyu.edu>
2021-03-17 13:39:16 -0400

    updating code starter

--- solution.py
+++ solution.py
@@ -11,11 +11,19 @@ def get_csv_data(filepath):
     :param filepath: The file path of the CSV data file to open
     :returns: A list of dictionaries, where each dictionary represents one row from the file
     """
+
+def remove_rows_with_blank_fields(data):
+    """
+    Removes any rows with one or more blank fields from the data set
+
+    :param data: The data, as a list of dictionaries
+    :returns: The modified data, as a list of dictionaries
+    """
     ## place your code here to complete this method according to the instructions above
 
-def remove_rows_with_blank_neighborhood_fields(data):
+def remove_rows_in_uk(data):
     """
-    Removes any rows with blank 'nta' or 'nta_code' fields from the data set
+    Removes any rows with 'United Kingdom' in the 'state' field.
 
     :param data: The data, as a list of dictionaries
     :returns: The modified data, as a list of dictionaries
@@ -23,9 +31,9 @@ def remove_rows_with_blank_neighborhood_fields(data):
     ## place your code here to complete this method according to the instructions above
 
 
-def remove_non_free_rows(data):
+def remove_rows_over_one_dollar(data):
     """
-    Removes any rows with anything other than 'Free' in the 'type' field.
+    Removes any rows with a value higher than 100 in the 'cost_per_impression' field.
 
     :param data: The data, as a list of dictionaries
     :returns: The modified data, as a list of dictionaries
@@ -33,25 +41,17 @@ def remove_non_free_rows(data):
     ## place your code here to complete this method according to the instructions above
 
 
-def make_location_title_case(data):
+def replace_email_domain(data, old_domain, new_domain):
     """
-    Puts the data in the 'location' field into Title Case, where the first letter of each word is capitalized.
+    Updates any rows where the 'email' ends in the old domain.  Updates to the new domain.
 
     :param data: The data, as a list of dictionaries
+    :param old_domain: The old domain to remove, e.g. '@dmoz.org'
+    :param new_domain: The new domain to replace the old_domain with, e.g. '@dmoz.com'
     :returns: The modified data, as a list of dictionaries
     """
     ## place your code here to complete this method according to the instructions above
 
-def fix_provider(data, old_provider, new_provider):
-    """
-    Swaps out the old provider name with the updated new provider name for any rows that match.
-
-    :param data: The data, as a list of dictionaries
-    :param old_provider: The old provider name to remove, e.g. 'SpotOnNetworks'
-    :param new_provider: The new domain to replace the old_domain with, e.g. 'Spot On Networks'
-    :returns: The modified data, as a list of dictionaries
-    """
-    ## place your code here to complete this method according to the instructions above
 
 def save_csv_data(data, filepath):
     """
@@ -62,13 +62,13 @@ def save_csv_data(data, filepath):
     """
     ## place your code here to complete this method according to the instructions above
 
-def get_number_free_hotspots(filepath, neighborhood):
+
+def get_average_cost_per_impression(filepath):
     """
     Calculates the average cost per impression of all records in the data set.
 
     :param filepath: The file path of the CSV data file to open
-    :param neighborhood: The neighborhood within which to count free wifi hotspots
-    :returns: The number of free wifi hotspots within the indicated neighborhood
+    :returns: The average cost per impression of all records in the data set
     """
     ## place your code here to complete this method according to the instructions above
 
@@ -83,20 +83,20 @@ def main():
     ## use the functions defined above to complete munging of the data file
 
     # get the data from the file
-    data = get_csv_data('data/wifi.csv')
+    data = get_csv_data('data/users.csv')
     
     # munge it
-    data = remove_rows_with_blank_neighborhood_fields(data)
-    data = remove_non_free_rows(data)
-    data = make_location_title_case(data)
-    data = fix_provider(data, 'SpotOnNetworks', 'Spot On Networks')
+    data = remove_rows_with_blank_fields(data)
+    data = remove_rows_in_uk(data)
+    data = remove_rows_over_one_dollar(data)
+    data = replace_email_domain(data, '@dmoz.org', '@dmoz.com')
 
     # dave to the new csv file
     save_csv_data(data, 'data/users_clean.csv')
 
     # print the average cost per impression from the data in the file
-    num = get_number_free_hotspots('data/wifi_clean.csv', 'Fort Greene')
-    print('There are ' + str(num) + ' free Wi-Fi hotspots in Fort Greene, Brooklyn.') 
+    avg = get_average_cost_per_impression('data/users_clean.csv')
+    print('The average cost per impression is:', format(avg, '.0f'), 'cents') # format number nicely
 
 if __name__ == "__main__":
     main()
