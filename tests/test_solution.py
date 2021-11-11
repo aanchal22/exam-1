848adb91ae3931452718d1cd362e13642a6f58ca
Aanchal Khandelwal <aan2210khandelwal@gmail.com>
2021-11-11 11:42:14 -0500

    Updating test cases for python part

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -29,11 +29,12 @@ def remove_invalid_handles(expected_users_data):
 
 users_data_filepath = os.path.join(os.getcwd(), "data/users.csv")
 expected_users_df = pd.read_csv(users_data_filepath)
-expected_users_df_non_null = expected_users_df.dropna()
+# expected_users_df_non_null = expected_users_df.dropna()
 
 expected_users_data = list(csv.DictReader(open(users_data_filepath, "r")))
 expected_users_data_non_null = remove_null_records(expected_users_data)
-
+expected_users_df_non_null = pd.DataFrame(expected_users_data_non_null)
+# 
 @pytest.fixture
 def state():
   return random.choice(expected_users_df["state"].unique())
@@ -62,14 +63,14 @@ class Tests:
     assert all([len(x) == expected_users_df.shape[1] for x in actual_users_data])
 
   def test_remove_rows_with_null_valued_fields_shape(self):
-    returned_data = remove_rows_with_null_valued_fields(expected_users_data)
+    returned_data = remove_rows_with_null_valued_fields(expected_users_data.copy())
     true_num_fields = len(expected_users_data[0])
     assert returned_data != None
     assert len(returned_data) == len(expected_users_data_non_null)
     assert all([len(x) == true_num_fields for x in returned_data])
 
   def test_remove_rows_with_null_valued_fields_value(self):
-    returned_data = remove_rows_with_null_valued_fields(expected_users_data)
+    returned_data = remove_rows_with_null_valued_fields(expected_users_data.copy())
     assert returned_data != None
     count = 0
     for record in returned_data:
@@ -79,16 +80,16 @@ class Tests:
     assert count == 0
 
   def test_remove_rows_with_invalid_handles_shape(self):
-    returned_data = remove_rows_with_invalid_handles(expected_users_data)
-    true_data = remove_invalid_handles(expected_users_data)
+    returned_data = remove_rows_with_invalid_handles(expected_users_data.copy())
+    true_data = remove_invalid_handles(expected_users_data.copy())
     check_len = len(true_data[0])
     assert returned_data != None
     assert len(returned_data) == len(true_data)
     assert all([len(x) == check_len for x in returned_data])
 
   def test_remove_rows_with_invalid_handles_value(self):
-    returned_data = remove_rows_with_invalid_handles(expected_users_data)
-    true_data = remove_invalid_handles(expected_users_data)
+    returned_data = remove_rows_with_invalid_handles(expected_users_data.copy())
+    true_data = remove_invalid_handles(expected_users_data.copy())
     assert returned_data != None
     assert len(returned_data) == len(true_data)
     assert returned_data == true_data
@@ -101,7 +102,7 @@ class Tests:
 
   def test_remove_rows_under_affinity_id_level_shape(self, affinity_type):
     threshold = int(random.choice(expected_users_df_non_null[~expected_users_df_non_null[affinity_type].isnull()][affinity_type].unique()))
-    actual_users_data_removed_affinity_threshold = remove_rows_over_affinity_id_level(expected_users_data_non_null, affinity_type, threshold)
+    actual_users_data_removed_affinity_threshold = remove_rows_over_affinity_id_level(expected_users_data_non_null.copy(), affinity_type, threshold)
     expected_users_df_non_null[affinity_type] = expected_users_df_non_null[affinity_type].astype(int)
     expected_users_df_removed_affinity_threshold = expected_users_df_non_null[expected_users_df_non_null[affinity_type] <= threshold]
 
@@ -112,7 +113,7 @@ class Tests:
   def test_remove_rows_over_affinity_id_level_value(self, affinity_type):
     threshold = int(random.choice(
       expected_users_df_non_null[~expected_users_df_non_null[affinity_type].isnull()][affinity_type].unique()))
-    returned_data = remove_rows_over_affinity_id_level(expected_users_data_non_null, affinity_type, threshold)
+    returned_data = remove_rows_over_affinity_id_level(expected_users_data_non_null.copy(), affinity_type, threshold)
 
     count = 0
     for record in returned_data:
@@ -122,15 +123,15 @@ class Tests:
     assert count == 0
 
   def test_replace_email_domain_shape(self, old_domain, new_domain):
-    actual_users_data_replaced_email = replace_email_domain(expected_users_data, old_domain, new_domain)
-    expected_users_df["email"] = expected_users_df["email"].str.replace(old_domain, new_domain)
+    actual_users_data_replaced_email = replace_email_domain(expected_users_data_non_null.copy(), old_domain, new_domain)
+    expected_users_df_non_null["email"] = expected_users_df_non_null["email"].str.replace(old_domain, new_domain)
 
     assert actual_users_data_replaced_email != None
-    assert len(actual_users_data_replaced_email) == expected_users_df.shape[0]
-    assert all([len(x) == expected_users_df.shape[1] for x in actual_users_data_replaced_email])
+    assert len(actual_users_data_replaced_email) == expected_users_df_non_null.shape[0]
+    assert all([len(x) == expected_users_df_non_null.shape[1] for x in actual_users_data_replaced_email])
 
   def test_replace_email_domain_value(self, old_domain, new_domain):
-    actual_users_data_replaced_email = replace_email_domain(expected_users_data, old_domain, new_domain)
+    actual_users_data_replaced_email = replace_email_domain(expected_users_data_non_null.copy(), old_domain, new_domain)
 
     count = 0
     for record in actual_users_data_replaced_email:
@@ -144,10 +145,11 @@ class Tests:
     assert os.stat(save_path).st_size > 100
 
   def test_get_average_and_median_affinity_id(self, affinity_type):
-    returned_average, returned_median = get_average_and_median_affinity_id(expected_users_data_non_null, affinity_type)
+    returned_average, returned_median = get_average_and_median_affinity_id(expected_users_data_non_null.copy(), affinity_type)
+    expected_users_df_non_null[affinity_type] = expected_users_df_non_null[affinity_type].astype(int)
     expected_affinity_average = expected_users_df_non_null[affinity_type].mean()
     expected_affinity_median = expected_users_df_non_null[affinity_type].median()
     assert returned_average != None
     assert returned_median != None
-    assert returned_average == expected_affinity_average
+    assert abs(returned_average - expected_affinity_average) < 10**-2
     assert returned_median == expected_affinity_median
